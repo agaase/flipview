d664b3acefeba9be3d719a33602633e2d09406e1
aseem <aseem@genwi.com>
2014-12-14 13:38:53 +0530

    fixing bugs and demo

--- src/flipview.js
+++ src/flipview.js
@@ -6,12 +6,16 @@
 
 var flipview = (function(){
     var slideIds = [-1,0,1];
-    var flipEleSelector = ".flipView";
-    var speedMapping = {"fast":400,"slow":2000};
+    var flipRuleId = 10;
+    var speedMapping = {"fast":700,"slow":2000,"default" : 1300};
+    var defaultMaxScale = 0.3;
 
     var fm = function(flipView,maxslides,options){
         this.params = options || {};
         this.flipArea = flipView.parent();
+        this.w = this.flipArea.width();
+        this.leftCutoff = flipView[0].getBoundingClientRect().left + parseInt(this.w/2-this.w/15,10);
+        this.rightCutoff = flipView[0].getBoundingClientRect().left + parseInt(this.w/2+this.w/15,10);
         this.flipEle = flipView;
         this.rotAngle = 0;
         this.maxSlides = maxslides;
@@ -21,16 +25,45 @@ var flipview = (function(){
         setupConfigurations.call(this);
     };
 
+    var transitionSlide = function(fromAngle, toAngle, dir){
+        fromAngle = fromAngle || (dir === -1 ? toAngle + 180 : toAngle - 180);
+        console.log("Rotating from angle - "+ fromAngle + " to angle - "+ toAngle);
+        var diff = Math.abs(Math.abs(fromAngle)-Math.abs(toAngle));
+        var level = parseInt(((diff-90)/180)*100,10);
+        var time = this.params.speed*level/100;
+        if(diff>90){
+            var halfAngle = dir === -1 ? toAngle + 90 : toAngle - 90;
+            this.flipEle.css({"-webkit-transform":"scale("+this.params.maxScale+") rotateY("+(halfAngle)+"deg)","transform":"scale("+this.params.maxScale+") rotateY("+(halfAngle)+"deg)","-webkit-transition-duration":time+"s","-webkit-transition-timing-function":"linear","transition-duration":time+"s","transition-timing-function":"linear"});        
+            setTimeout(function(){
+                this.flipEle.css({"-webkit-transform":"scale(1) rotateY("+(toAngle)+"deg)","transform":"scale(1) rotateY("+(toAngle)+"deg)","-webkit-transition-duration":(this.params.speed-time)+"s","-webkit-transition-timing-function":"linear","transition-duration":(this.params.speed-time)+"s","transition-timing-function":"linear"});        
+            }.bind(this),time*1000);
+        }else{
+            var dur = (this.params.speed/180)*(Math.abs(fromAngle - toAngle))*1000;
+            setTimeout(function(){
+                this.flipEle.css({"-webkit-transform":"rotateY("+(toAngle)+"deg)","transform":"rotateY("+(toAngle)+"deg)","-webkit-transition-duration":this.params.speed+"s","-webkit-transition-timing-function":"linear","transition-duration":this.params.speed+"s","transition-timing-function":"linear"});        
+            }.bind(this),dur);
+        }
+    };
+
     /**
      * Sets up the various configurational parameters
      * @method setupConfigurations
      */
     var setupConfigurations = function(){
-        var speed = parseInt(this.params.speed);
+        var speed = parseInt(this.params.speed,10);
+        var defaultSpeed = window.innerWidth > 700 ? speedMapping[this.params.speed] : 500; 
         if(!speed){
-            speed = ((typeof(this.params.speed) === "string" ? speedMapping[this.params.speed] : 1000 ) || 1000);
+            speed = ((typeof(this.params.speed) === "string" ? speedMapping[this.params.speed] : defaultSpeed ) || defaultSpeed);
+
         }
-        this.params.speed = speed/1000;
+
+        if(!speed){
+            speed = typeof(this.params.speed) === "string" ? speed : "default";
+            speed = speedMapping[speed] || speedMapping["default"];
+            speed = window.innerWidth<700 ? speed/1.5 : speed;
+        }
+        this.params.speed = (speed/1000);
+        this.params.maxScale = typeof(this.params.maxScale) === "number" ? this.params.maxScale : defaultMaxScale;
     };
 
     /**
@@ -38,12 +71,12 @@ var flipview = (function(){
      * @method setupLayout
      */
     var setupLayout = function(){
-        this.flipEle.css("position","relative").append("<div class='front'></div>").append("<div class='next'></div>").append($("<div class='prev'></div>"));
-        this.flipArea.css("-webkit-perspective","1000");
-        this.flipEle.css("-webkit-transform-style","preserve-3d");
-        $(flipEleSelector +" > div").css({"-webkit-backface-visibility":"hidden","position":"absolute","height":"100%","width":"100%"}).addClass("flipSlide");
-        $(".next,.prev",this.flipEle).hide().css("-webkit-transform","rotateY(-180deg)");
-    };
+        this.flipEle.css("position","relative").append("<div class='front'></div>").append("<div class='next'></div>").append("<div class='prev'></div>");
+        this.flipArea.css({"-webkit-perspective":"1000","perspective" : "1000"});
+        this.flipEle.css({"-webkit-transform-style":"preserve-3d","transform-style":"preserve-3d"});
+        $("> div",this.flipEle).css({"-webkit-backface-visibility":"hidden","backface-visibility":"hidden","position":"absolute","height":"100%","width":"100%"}).addClass("flipSlide");
+        $(".next,.prev",this.flipEle).hide().css({"-webkit-transform":"rotateY(-180deg)","transform":"rotateY(-180deg)"});
+    };  
 
     /**
      * Binds touch events to calculate the exact logic for doing flipping and panning.
@@ -52,44 +85,61 @@ var flipview = (function(){
      */
     var bindFlipEvent = function(){
         var minimumDisplPossible = 10;
-        var touchEndTimer, touchMoveTimer, startPosX, lastPosX, firstTouchMove = true;
+        var touchEndTimer, touchMoveTimer, startPosX, lastPosX, firstTouchMove = true, touchStarted = false;
         var handleTouchstart = function(ev){
-                var touchobj = ev.originalEvent.changedTouches[0];
+                if(!this.flipLock){
+                    touchStarted = true;
+                }
+                //var touchobj = ev.originalEvent.changedTouches[0];
+                var touchobj = ev.originalEvent.type === "mousedown" ? ev.originalEvent : ev.originalEvent.changedTouches[0];
                 startPosX = touchobj.pageX;
                 lastPosX = touchobj.pageX;
             }.bind(this);
         var handleTouchmove = function(ev){
             ev.preventDefault();
-            var touchobj = ev.originalEvent.changedTouches[0];
-            if(Math.abs(touchobj.pageX-lastPosX)<minimumDisplPossible){
-                //return;
+            if(touchMoveTimer){
+                clearTimeout(touchMoveTimer);
             }
-            lastPosX = touchobj.pageX;
-            if((startPosX < 550 && this.slideIds[0] !==-1) || (startPosX > 650 && this.slideIds[2] !==-1)){
-                    if(firstTouchMove){
-                        if(startPosX < 550){
-                            $(".prev",this.flipEle).show();
-                            $(".next",this.flipEle).hide();
-                        }else if(startPosX > 650){
-                            $(".next",this.flipEle).show();
-                            $(".prev",this.flipEle).hide();
+            touchMoveTimer = setTimeout(function(){
+                var touchobj = ev.originalEvent.type === "mousemove" ? ev.originalEvent : ev.originalEvent.changedTouches[0];
+                if(!touchStarted){
+                    return;
+                }
+                if(Math.abs(touchobj.pageX-lastPosX)<minimumDisplPossible){
+                    //return;
+                }
+                lastPosX = touchobj.pageX;
+                if((startPosX < this.leftCutoff && this.slideIds[0] !==-1) || (startPosX > this.rightCutoff && this.slideIds[2] !==-1)){
+                        if(firstTouchMove){
+                            if(startPosX < this.leftCutoff){
+                                $(".prev",this.flipEle).show();
+                                $(".next",this.flipEle).hide();
+                            }else if(startPosX > this.rightCutoff){
+                                $(".next",this.flipEle).show();
+                                $(".prev",this.flipEle).hide();
+                            }
+                            pan.call(this,(touchobj.pageX-startPosX)/2,startPosX < this.leftCutoff ? 1 : -1);
+                            firstTouchMove=false;
                         }
-                        setTimeout(function(){
-                            pan.call(this,(touchobj.pageX-startPosX)/2,startPosX < 550 ? 1 : -1);
-                        }.bind(this),50);
-                        firstTouchMove=false;
-                    }
-                    pan.call(this,(touchobj.pageX-startPosX)/3,startPosX < 550 ? 1 : -1);
-            }
+                        pan.call(this,(touchobj.pageX-startPosX)/3,startPosX < this.leftCutoff ? 1 : -1);
+                }
+            }.bind(this),firstTouchMove ? 30 : 5);
         }.bind(this);
         var handleTouchend = function(ev){
+                if(!touchStarted){
+                    return;
+                }
                 ev.preventDefault();
                 if(touchEndTimer){
                     clearTimeout(touchEndTimer);
                 }
+                if(touchMoveTimer){
+                    clearTimeout(touchMoveTimer);
+                }
                 touchEndTimer = setTimeout(function(){
-                    var touchobj = ev.originalEvent.changedTouches[0];
-                    if((touchobj.pageX-startPosX)<-100 && startPosX > 800 && this.slideIds[2] !==-1) {
+                    touchStarted = false;
+                    var touchobj = ev.originalEvent.type === "mouseup" ? ev.originalEvent : ev.originalEvent.changedTouches[0];
+                    if((touchobj.pageX-startPosX)<-100 && startPosX > this.rightCutoff && this.slideIds[2] !==-1) {
                         if(firstTouchMove){
                             $(".next",this.flipEle).show();
                             $(".prev",this.flipEle).hide();
@@ -99,7 +149,7 @@ var flipview = (function(){
                             firstTouchMove=true;
                         }.bind(this),50);
                         firstTouchMove=true;
-                    }else if((touchobj.pageX-startPosX)>100 && startPosX < 400 && this.slideIds[0] !== -1){
+                    }else if((touchobj.pageX-startPosX)>100 && startPosX < this.leftCutoff && this.slideIds[0] !== -1){
                         if(firstTouchMove){
                             $(".prev",this.flipEle).show();
                             $(".next",this.flipEle).hide();
@@ -117,6 +167,7 @@ var flipview = (function(){
                 }.bind(this),50);
             }.bind(this);
         this.flipArea.on('touchstart',handleTouchstart).on('touchmove',handleTouchmove).on('touchend',handleTouchend);
+        this.flipArea.on('mousedown',handleTouchstart).on('mousemove',handleTouchmove).on('mouseup',handleTouchend);
     };
 
     /**
@@ -153,24 +204,34 @@ var flipview = (function(){
      * @return {[type]}
      */
     var flip = function(direction){
+            this.flipLock = true;
+            setTimeout(function(){
+                //Locking any touch events until the flip is completed.
+                //Not doing this is just too much of complexity.
+                this.flipLock = false;
+            }.bind(this),this.params.speed*1000);
             if(this.beforeFlip){
+                //Callback function before calling flip.
                 this.beforeFlip(direction);
             }
+            var oldA = this.rotAngle;
             this.rotAngle += (direction * 180);
             var ang = this.rotAngle;
-            this.flipEle.css({"-webkit-transform":"rotateY("+(ang)+"deg)","transition-duration":this.params.speed+"s","transition-timing-function":"linear"});        
+            
+            transitionSlide.call(this,this.panAngle,this.rotAngle,direction);
+            this.panAngle = undefined;
             setTimeout(function(){
                 //Draw the next set of articles; well only if something is there to draw
                 setSlides.call(this,direction);
-                if(direction<0 && this.slideIds[2] > -1){
+                
+                if(direction<0){
                     this.onFlipNext();
-                    $(".next",this.flipEle).css("-webkit-transform","rotateY("+((this.slideIds[2]%2)*180)+"deg)");
-
-                }else if(direction>0 && this.slideIds[0] > -1){
+                    $(".next",this.flipEle).css({"-webkit-transform":"rotateY("+((this.slideIds[2]%2)*180)+"deg)","transform":"rotateY("+((this.slideIds[2]%2)*180)+"deg)"});
+                }else if(direction>0){
                     this.onFlipPrev();
-                    $(".prev",this.flipEle).css("-webkit-transform","rotateY("+((this.slideIds[2]%2)*180)+"deg)");
+                    $(".prev",this.flipEle).css({"-webkit-transform":"rotateY("+((this.slideIds[2]%2)*180)+"deg)","transform":"rotateY("+((this.slideIds[2]%2)*180)+"deg)"});
                 }
-            }.bind(this),1000);
+            }.bind(this),this.params.speed*1000);
     };
 
     /**
@@ -182,8 +243,8 @@ var flipview = (function(){
     var getScaleValue = function(angleRotated){
         angleRotated = Math.min(Math.abs(angleRotated),180);
         angleRotated = angleRotated > 90 ? 90-angleRotated : angleRotated;
-        var maxScale = this.params.maxScale/100 || 0.4;
-        return(1 - (angleRotated >= 0 ? (angleRotated/90)*maxScale : maxScale + (angleRotated/90)*maxScale));
+        var scale = 1-(this.params.maxScale) ;
+        return(1 - (angleRotated >= 0 ? (angleRotated/90)*scale : scale + (angleRotated/90)*scale));
     };
     
     /**
@@ -193,20 +254,30 @@ var flipview = (function(){
      * @param  {number} direction     -1 clockwise, 1 anticlockwise
      */
     var pan = function(angleToRotate,direction){
+            if(this.flipLock){
+                return;
+            }
+            angleToRotate = angleToRotate * 2000/window.innerWidth;
             var angle = this.rotAngle+angleToRotate;
+
             if(direction>0){
+                this.panAngle = angle;
                 angle = angle < this.rotAngle ? this.rotAngle : angle;
-                if(angle - this.rotAngle > 180){
+                if(angle - this.rotAngle >= 180){
                     angle = this.rotAngle + 180;
                 }
             }else if(direction <= 0){
+                this.panAngle = angle;
                 angle = angle > this.rotAngle ? this.rotAngle : angle;
-                if(this.rotAngle - angle  > 180){
+                if(this.rotAngle - angle  >= 180){
                     angle = this.rotAngle - 180;
                 }
             }
+
+            this.panAngle = angle;
             var dur = (this.params.speed/180)*(Math.abs(angle - this.rotAngle));
-            this.flipEle.css({"-webkit-transform":"scale("+getScaleValue.call(this,angleToRotate)+") rotateY("+angle+"deg)","transition-duration":""+dur+"s","transition-timing-function":"linear"});    
+            var currScale = getScaleValue.call(this,angleToRotate);
+            this.flipEle.css({"-webkit-transform":"scale("+currScale+") rotateY("+angle+"deg)","transform":"scale("+currScale+") rotateY("+angle+"deg)","-webkit-transition-duration":""+dur+"s","-webkit-transition-timing-function":"linear","transition-duration":""+dur+"s","transition-timing-function":"linear"});    
     };
    return fm;
 })();
